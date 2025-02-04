af83363fc0a4d018ae406bb1aceff898cdbf8828
Israel <u23271982@utp.edu.pe>
2025-02-03 19:50:56 -0500

    Se arreglo las identidades modelos

--- src/main/java/cont/com/model/Itinerary.java
+++ src/main/java/cont/com/model/Itinerary.java
@@ -1,2 +1,34 @@
-package cont.com.model;public class Itinerary {
+package cont.com.model;
+
+import java.util.List;
+
+public class Itinerary {
+    private Long id;
+    private List<Segment> segment;
+
+    private Price price;
+
+    public List<Segment> getSegment() {
+        return segment;
+    }
+
+    public void setSegment(List<Segment> segment) {
+        this.segment = segment;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    public void setPrice(Price price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
